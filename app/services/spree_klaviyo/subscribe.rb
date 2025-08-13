baa2f2c6493c80f7d3b70a09d642719a323f8377
Jawaria Waris <jawaria.waris@sparksolutions.co>
2025-08-13 05:55:11 +0500

    Add custom-properties support to Klaviyo Subscribe & Profile services

--- app/services/spree_klaviyo/subscribe.rb
+++ app/services/spree_klaviyo/subscribe.rb
@@ -2,11 +2,23 @@ module SpreeKlaviyo
   class Subscribe < Base
     prepend ::Spree::ServiceModule::Base
 
-    def call(klaviyo_integration:, email:, user: nil)
+    def call(klaviyo_integration:, email:, user: nil, custom_properties: {})
       return failure(false, ::Spree.t('admin.integrations.klaviyo.not_found')) unless klaviyo_integration
 
       klaviyo_integration.subscribe_user(email).tap do |result|
-        user.update(klaviyo_subscribed: true) if result.success? && user && !user.klaviyo_subscribed?
+        if result.success?
+          user&.update(klaviyo_subscribed: true) if user && !user.klaviyo_subscribed?
+
+          if custom_properties.present?
+            target_user = user || ::Spree.user_class.find_or_initialize_by(email: email)
+
+            SpreeKlaviyo::CreateOrUpdateProfile.call(
+              klaviyo_integration: klaviyo_integration,
+              user: target_user,
+              custom_properties: custom_properties
+            )
+          end
+        end
       end
     end
   end
