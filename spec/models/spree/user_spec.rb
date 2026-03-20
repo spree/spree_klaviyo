0c066135081494ec9a390250677cf4f679379726
Szymon Chorobski <szymon.chorobski@getvendo.com>
2026-03-20 15:51:38 +0100

    rm user.klaviyo_subscribed?

--- spec/models/spree/user_spec.rb
+++ spec/models/spree/user_spec.rb
@@ -1,17 +1,39 @@
 require 'spec_helper'
 
 RSpec.describe Spree.user_class, type: :model do
-  describe 'Klaviyo' do
-    let(:user) { build(:user) }
+  describe 'Klaviyo related methods' do
+    let(:user) { build(:user, klaviyo_id: klaviyo_id_value, klaviyo_visitor_id: klaviyo_visitor_id_value) }
 
-    describe '#klaviyo_subscribed?' do
-      it 'returns false by default' do
-        expect(user.klaviyo_subscribed?).to be(false)
+    let(:klaviyo_id_value) { nil }
+    let(:klaviyo_visitor_id_value) { nil }
+
+    describe '#klaviyo_id' do
+      subject(:klaviyo_id) { user.klaviyo_id }
+
+      context 'when not set' do
+        let(:klaviyo_id_value) { nil }
+
+        it { is_expected.to be_nil }
       end
 
-      it 'returns true when set' do
-        user.klaviyo_subscribed = true
-        expect(user.klaviyo_subscribed?).to be(true)
+      context 'when set' do
+        let(:klaviyo_id_value) { '123' }
+
+        it { is_expected.to eq('123') }
+      end
+    end
+
+    describe '#klaviyo_visitor_id' do
+      subject(:klaviyo_visitor_id) { user.klaviyo_visitor_id }
+
+      context 'when not set' do
+        it { is_expected.to be_nil }
+      end
+
+      context 'when set' do
+        let(:klaviyo_visitor_id_value) { '123' }
+
+        it { is_expected.to eq('123') }
       end
     end
   end
