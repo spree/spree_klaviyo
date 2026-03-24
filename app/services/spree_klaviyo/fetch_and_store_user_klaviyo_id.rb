20f4a0eb38d0e22bc5fe00d13539a201a3e1986a
Szymon Chorobski <szymon.chorobski@getvendo.com>
2026-03-24 23:17:11 +0100

    rabbit fixes

--- app/services/spree_klaviyo/fetch_and_store_user_klaviyo_id.rb
+++ app/services/spree_klaviyo/fetch_and_store_user_klaviyo_id.rb
@@ -8,8 +8,8 @@ module SpreeKlaviyo
 
       if user.klaviyo_id.present?
         return success(user.klaviyo_id)
-      elsif fetch_profile.success?
-        user.update!(klaviyo_id: JSON.parse(fetch_profile.value)['data'][0]['id'])
+      elsif fetch_profile.success? && fetched_klaviyo_id.present?
+        user.update!(klaviyo_id: fetched_klaviyo_id)
         return success(user.klaviyo_id)
       else
         return failure(false, fetch_profile.value)
@@ -22,6 +22,12 @@ module SpreeKlaviyo
 
     def fetch_profile
       @fetch_profile ||= klaviyo_integration.fetch_profile(email: user.email)
+    end
+
+    def fetched_klaviyo_id
+      return @fetched_klaviyo_id if instance_variable_defined?(:@fetched_klaviyo_id)
+
+      @fetched_klaviyo_id = JSON.parse(fetch_profile.value).dig('data', 0, 'id')
     end
   end
 end
