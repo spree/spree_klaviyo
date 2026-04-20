0d7283664b2ca74a3f365132e1db3ff870e8c040
Kacper Mekarski <kacper.mekarski@gmail.com>
2026-04-20 14:08:56 +0200

    VCR config clean up

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -10,25 +10,23 @@ VCR.configure do |c|
   c.cassette_library_dir = File.join(SpreeKlaviyo::Engine.root, 'spec', 'vcr')
   c.hook_into :webmock
   c.ignore_localhost = true
+  c.register_request_matcher :normalized_body do |req1, req2|
+    normalize = ->(body) do
+      parsed = JSON.parse(body)
+      list_data = parsed.dig("data", "relationships", "list", "data")
+      list_data["id"] = "PLACEHOLDER" if list_data&.key?("id")
+      parsed
+    rescue JSON::ParserError
+      body
+    end
+
+    normalize.call(req1.body) == normalize.call(req2.body)
+  end
   c.default_cassette_options = {
     allow_unused_http_interactions: false,
     record: :once,
-    match_requests_on: [:method, :uri, :body]
+    match_requests_on: [:method, :uri, :normalized_body]
   }
-  VCR.configure do |config|
-    config.register_request_matcher :body do |req1, req2|
-      normalize = ->(body) do
-        parsed = JSON.parse(body)
-        list_data = parsed.dig("data", "relationships", "list", "data")
-        list_data["id"] = "PLACEHOLDER" if list_data&.key?("id")
-        parsed
-      rescue JSON::ParserError
-        body
-      end
-
-      normalize.call(req1.body) == normalize.call(req2.body)
-    end
-  end
   c.filter_sensitive_data('<KLAVIYO_PUBLIC_API_KEY>') { SpreeKlaviyo::Testing.default_public_api_key }
   c.filter_sensitive_data('<KLAVIYO_PRIVATE_API_KEY>') { SpreeKlaviyo::Testing.default_private_api_key }
   c.filter_sensitive_data('KLAVIYO_DEFAULT_NEWSLETTER_LIST_ID') { SpreeKlaviyo::Testing.default_newsletter_list_id }
