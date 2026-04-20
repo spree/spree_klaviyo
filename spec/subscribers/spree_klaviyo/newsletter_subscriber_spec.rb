b6d91f80536732fad8ebb3b4d74d7c3193c79ebe
Kacper Mekarski <kacper.mekarski@gmail.com>
2026-04-20 17:26:57 +0200

    check arguments as well

--- spec/subscribers/spree_klaviyo/newsletter_subscriber_spec.rb
+++ spec/subscribers/spree_klaviyo/newsletter_subscriber_spec.rb
@@ -11,10 +11,13 @@ RSpec.describe SpreeKlaviyo::NewsletterSubscriber do
   describe '#newsletter_subscriber.created event' do
     let(:email) { 'customer@example.com' }
 
-    it 'enqueues SubscribeJob when a newsletter subscriber is created via checkout' do
+    it 'enqueues SubscribeJob with correct arguments' do
       expect {
         Spree::Newsletter::Subscribe.new(email: email).call
-      }.to have_enqueued_job(SpreeKlaviyo::SubscribeJob)
+      }.to have_enqueued_job(SpreeKlaviyo::SubscribeJob).with { |integration_id, subscriber_id|
+        expect(integration_id).to eq(klaviyo_integration.id)
+        expect(Spree::NewsletterSubscriber.find(subscriber_id).email).to eq(email)
+      }
     end
 
     it 'does not enqueue SubscribeJob when subscriber already exists' do
@@ -39,10 +42,10 @@ RSpec.describe SpreeKlaviyo::NewsletterSubscriber do
   describe '#newsletter_subscriber.deleted event' do
     let!(:existing_subscriber) { create(:newsletter_subscriber) }
 
-    it 'enqueues UnsubscribeJob when a newsletter subscriber is destroyed' do
+    it 'enqueues UnsubscribeJob with correct arguments' do
       expect {
         existing_subscriber.destroy!
-      }.to have_enqueued_job(SpreeKlaviyo::UnsubscribeJob)
+      }.to have_enqueued_job(SpreeKlaviyo::UnsubscribeJob).with(klaviyo_integration.id, existing_subscriber.email)
     end
 
     context 'without klaviyo integration' do
